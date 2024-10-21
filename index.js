4caf73ff0b506cd4e8ffa35a6e51a1308161a226
Beaconyx2025 <hongallenthiver2018@gmail.com>
2024-10-21 16:21:50 +0900

    Chapter 11-13

--- index.js
+++ index.js
@@ -1,83 +1,100 @@
-const arr = [1, 2, 3];
-arr.forEach(console.log);
+// const arr = [1, 2, 3];
+// arr.forEach(console.log);
 
-// Chat 4
+// // Chat 4
 
-console.log(score);
+// console.log(score);
 
-score = 90;
-var score;
-score = "afd";
-console.log(score);
+// score = 90;
+// var score;
+// score = "afd";
+// console.log(score);
 
-let a = 10,
-  b = 20,
-  c = 30;
-var answer = 40;
+// let a = 10,
+//   b = 20,
+//   c = 30;
+// var answer = 40;
 
-if (a >= b) {
-  answer = a;
-} else if (b > a) {
-  answer = b;
+// if (a >= b) {
+//   answer = a;
+// } else if (b > a) {
+//   answer = b;
+// }
+// if (c > answer) {
+//   answer = c;
+// }
+
+// console.log(answer);
+
+// console.log(1 === 1.0);
+
+// var key = Symbol("key");
+// console.log(typeof key);
+
+// let num = 42;
+// let str = "Hello";
+// let isTrue = true;
+
+// console.log(typeof num); // "number"
+// console.log(typeof str); // "string"
+// console.log(typeof isTrue); // "boolean"
+// console.log(typeof {}); // "object"
+// console.log(typeof []); // "object" (배열도 객체로 간주)
+// console.log(typeof null); // "object" (자바스크립트의 오래된 버그)
+// console.log(typeof undefined); // "undefined"
+
+// if (!"") console.log("" + " is falsy value");
+
+// console.log("0" && "0");
+
+// function getStringLength(str) {
+//   str = str || "";
+//   return str.length;
+// }
+
+// console.log(getStringLength());
+// console.log(getStringLength("hello"));
+
+// var w = "hello";
+
+// var length = w?.length;
+// console.log(length);
+
+// var foo = "1" ?? "default string";
+// console.log(foo);
+
+// var obj = {};
+// var key = "hello";
+
+// obj[key] = "world";
+
+// console.log(obj);
+
+// var person = {
+//   last_name: "lee",
+//   1: 10,
+// };
+
+// console.log(person.last_name);
+
+// var x = 1,
+//   y = 2;
+// const test = { x, y };
+// console.log(test);
+
+let x = 1;
+
+function foo() {
+  if ((x = 1)) {
+    x = 10;
+  }
+
+  bar();
 }
-if (c > answer) {
-  answer = c;
+
+function bar() {
+  console.log(x);
 }
 
-console.log(answer);
-
-console.log(1 === 1.0);
-
-var key = Symbol("key");
-console.log(typeof key);
-
-let num = 42;
-let str = "Hello";
-let isTrue = true;
-
-console.log(typeof num); // "number"
-console.log(typeof str); // "string"
-console.log(typeof isTrue); // "boolean"
-console.log(typeof {}); // "object"
-console.log(typeof []); // "object" (배열도 객체로 간주)
-console.log(typeof null); // "object" (자바스크립트의 오래된 버그)
-console.log(typeof undefined); // "undefined"
-
-if (!"") console.log("" + " is falsy value");
-
-console.log("0" && "0");
-
-function getStringLength(str) {
-  str = str || "";
-  return str.length;
-}
-
-console.log(getStringLength());
-console.log(getStringLength("hello"));
-
-var w = "hello";
-
-var length = w?.length;
-console.log(length);
-
-var foo = "1" ?? "default string";
-console.log(foo);
-
-var obj = {};
-var key = "hello";
-
-obj[key] = "world";
-
-console.log(obj);
-
-var person = {
-  last_name: "lee",
-  1: 10,
-};
-
-console.log(person.last_name);
-
-var x = 1,
-  y = 2;
-const test = { x, y };
-console.log(test);
+foo();
+bar();
